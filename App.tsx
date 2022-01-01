cccfc792d76e9ca2f427f28429d0645ac4093276
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2022-01-02 00:34:54 +0400

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AnimatedSquare from "./components/AnimatedSquare";
+import ScrollItemAnimation from "./components/ScrollItemAnimation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return <ScrollItemAnimation />;
+	// return <AnimatedSquare />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
